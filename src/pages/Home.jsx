3049234f26d2d1385616a4baaa9273112ef8589e
anushaladha04 <anushaladha04@gmail.com>
2025-11-11 17:59:21 -0800

    loader + added drei

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber' 
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
@@ -7,7 +8,14 @@ import React from 'react'
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-      
+      <Canvas 
+        className="w-full h-full bg-transparent"
+        camera={{near: 0.1, far: 1000}}
+        >
+          <Suspense fallback={<Loader />}>
+
+          </Suspense>
+      </Canvas>
     </section>
   )
 }
