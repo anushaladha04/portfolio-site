b4d2cd7e22626988b35210b175e3bff889216cb7
anushaladha04 <anushaladha04@gmail.com>
2025-11-24 13:32:06 -0800

    made homeinfo file

---
+++ src/components/HomeInfo.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const renderContent = {
+    1: (
+        <h1>1</h1>
+    ),
+    2: (
+        <h1>2</h1>
+    ), 
+    3: (
+        <h1>3</h1>
+    ),
+    4: (
+        <h1>4</h1>
+    ),
+}
+
+const infoBox = ({
+    
+})
+
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+}
+
+export default HomeInfo
