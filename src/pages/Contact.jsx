aaea03dca2bfd565360b5e18af0330121d20a252
anushaladha04 <anushaladha04@gmail.com>
2025-11-24 18:31:25 -0800

    added email js dependencies + altered contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react';
 
 const Contact = () => {
   const [form, setForm] = useState({name: '', email: '', message: ''});
-
+  const [isLoading, setIsLoading] = useState(false);
+  
   const handleChange = () => {};
+  const handleFocus = () => {};
+  const handleBlur = () => {};
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
@@ -19,13 +22,52 @@ const Contact = () => {
             <input 
               type="text"
               name="name"
-              placeholder="Enter your name"
+              placeholder="John Doe"
               className="input"
               required
               value={form.name}
               onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
             />
           </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input 
+              type="email"
+              name="email"
+              placeholder="john@gmail.com"
+              className="input"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea 
+              name="message"
+              rows={4}
+              placeholder="Let me know how I can help you!"
+              className="textarea"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
         </form>
       </div>
     </section>
