4ac7238046aa5d5690fc7ead5e791cee31de277d
anushaladha04 <anushaladha04@gmail.com>
2025-11-24 18:18:36 -0800

    basic contact setup

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Contact = () => {
+  const [form, setForm] = useState({name: '', email: '', message: ''});
+
+  const handleChange = () => {};
+
   return (
-    <div>Contact</div>
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get In Touch</h1>
+
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+        >
+          <label className="text-black-500 font-semibold">
+            Name
+            <input 
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="input"
+              required
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+        </form>
+      </div>
+    </section>
   )
 }
 
