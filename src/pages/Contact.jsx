813b46385742e56b5b4c3b910a398c7c655eb625
anushaladha04 <anushaladha04@gmail.com>
2025-11-25 19:54:21 -0800

    email sending working

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,15 +14,15 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    emailjs.sendForm(
+    emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       {
         from_name: form.name,
-        to_name: 'Anusha Ladha',
         from_email: form.email,
-        to_email: 'anushaladha4@g.ucla.edu',
         message: form.message,
+        to_name: 'Anusha Ladha',
+        to_email: 'anushaladha4@g.ucla.edu',
       },
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
     ).then(() => {
